908939a7db0757316ad81ebe33e61eda207eead7
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-03 18:05:46 -0800

    Improve error handling

--- server.py
+++ server.py
@@ -28,7 +28,6 @@ class ReferenceCollector(ast.NodeVisitor):
 
     def visit(self, node):
         super().visit(node)
-        LOGGER.debug('visiting node')
         return self.use_count
 
     def noop(self):
@@ -45,23 +44,26 @@ class ReferenceCollector(ast.NodeVisitor):
         self.__add_to_counter(node.id, allow_unrecognized = False)
 
 async def code_to_module_uses(json_object):
-    LOGGER.info('got a json object...{}'.format(str(json_object)))
     try:
         code = base64.b64decode(json_object['code'].encode('utf-8'))
-        LOGGER.info('about to parse code {}'.format(code))
         data = { 'use_count': ReferenceCollector().visit(ast.parse(code)) }
     except KeyError as exc:
-        data = { 'error': errno.EINVAL, 'use_count': None }
+        data = { 'error': errno.EINVAL, 'use_count': {} }
     except ValueError as exc:
-        if exc.message.find('source code string cannot contain null bytes') >= 0:
+        if exc.args[0].find('source code string cannot contain null bytes') >= 0:
             LOGGER.error('Skipping parsing because code contains null bytes!')
-            data = { 'error': errno.EPERM, 'use_count': None }
+            # with open('myexamplefile.py', 'wb') as f:
+            #     f.write(code)
+            data = { 'error': errno.EPERM, 'use_count': {} }
         else:
             LOGGER.exception('Unhandled exception!')
-            data = { 'error': errno.EIO, 'use_count': None }
+            data = { 'error': errno.EIO, 'use_count': {} }
+    except SyntaxError as exc:
+        LOGGER.warning('Invalid Python3 code received: ' + str(exc.msg))
+        data = { 'error': errno.EIO, 'use_count': {} }
     except Exception as exc:
         LOGGER.exception('Unhandled exception')
-        data = { 'error': errno.EIO, 'use_count': None }
+        data = { 'error': errno.EIO, 'use_count': {} }
     return json.dumps(data)
 
 if __name__ == '__main__':

5561a9f2a8c67307ee24f3b9a3ecdf9c3e70e838
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-15 20:51:57 -0800

    Update grammar to properly act as module

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ class ReferenceCollector(ast.NodeVisitor):
 
     def visit(self, node):
         super().visit(node)
-        self.use_count.update(['__grammar__'+node.__class__.__name__])
+        self.use_count.update(['__grammar__.' + node.__class__.__name__])
         return self.use_count
 
     def noop(self):
